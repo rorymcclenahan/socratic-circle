6984b2106ccedbb00e200662adac760d95be9772
rorymcc8898 <98715020+rorymcclenahan@users.noreply.github.com>
2024-03-31 01:35:09 -0400

    Add TeacherClassEditor and cleanup Teacher

--- socratic-circle-frontend/src/views/Teacher.js
+++ socratic-circle-frontend/src/views/Teacher.js
@@ -1,27 +1,59 @@
 import React from "react";
 import "../styles/iframe.css";
 import TeacherDocViewer from "../components/TeacherDocViewer";
-import TeacherManageGroups from "../components/TeacherManageGroups";
+import TeacherGroupModal from "../components/TeacherGroupModal";
+import TeacherClassModal from "../components/TeacherClassModal";
 
 const Teacher = () => {
-  const [showTeacherDocViewer, setShowTeacherDocViewer] = React.useState(true);
+  const [showTeacherDocViewer, setShowTeacherDocViewer] = React.useState(false);
+  const [showTeacherGroupModal, setShowTeacherGroupModal] =
+    React.useState(false);
+  const [showTeacherClassModal, setShowTeacherClassModal] =
+    React.useState(true);
 
   const toggleTeacherDocViewer = () => {
     setShowTeacherDocViewer(!showTeacherDocViewer);
   };
+
+  const toggleTeacherGroupModal = () => {
+    setShowTeacherGroupModal(!showTeacherGroupModal);
+  };
+
+  const toggleTeacherClassModal = () => {
+    setShowTeacherClassModal(!showTeacherClassModal);
+  };
+
   return (
     <div>
-      <button onClick={toggleTeacherDocViewer}>
-        Toggle Teacher Doc Viewer
-      </button>
+      <div className="toggle-view-buttons">
+        <button onClick={toggleTeacherDocViewer}>
+          Toggle Teacher Doc Viewer
+        </button>
+        <button onClick={toggleTeacherGroupModal}>
+          Toggle Teacher Manage Groups
+        </button>
+        <button onClick={toggleTeacherClassModal}>
+          Toggle Teacher Manage Class
+        </button>
+      </div>
+
       {showTeacherDocViewer && (
         <div className="Teacher-Doc-Container">
           <TeacherDocViewer />
         </div>
       )}
-      <div>
-        <TeacherManageGroups />
-      </div>
+
+      {showTeacherGroupModal && (
+        <div className="Teacher-Groups-Container">
+          <TeacherGroupModal />
+        </div>
+      )}
+
+      {showTeacherClassModal && (
+        <div className="Teacher-Create-Class-Container">
+          <TeacherClassModal />
+        </div>
+      )}
     </div>
   );
 };
