f183f22e9e4b0c0ed2b5bf322db3bd809e377897
rorymcc8898 <98715020+rorymcclenahan@users.noreply.github.com>
2024-03-30 22:40:59 -0400

    Add multi view function to TeacherDocView

--- socratic-circle-frontend/src/components/TeacherDocViewer.js
+++ socratic-circle-frontend/src/components/TeacherDocViewer.js
@@ -2,25 +2,25 @@ import React from "react";
 
 const TeacherDocViewer = (props) => {
   // TODO: Implement API call to backend to get the teacher's documents
-const docUrls = [
+  const docUrls = [
     {
-        groupName: "Group A",
-        docIndex: 0,
-        url: "https://docs.google.com/document/d/1OulQL0EtHAiTl7DEmLF77ApexEVsIQlwWdWr-4UQfhw/edit?usp=sharing",
+      groupName: "Group A",
+      docIndex: 0,
+      url: "https://docs.google.com/document/d/1OulQL0EtHAiTl7DEmLF77ApexEVsIQlwWdWr-4UQfhw/edit?usp=sharing",
     },
     {
-        groupName: "Group B",
-        docIndex: 1,
-        url: "https://docs.google.com/document/d/1RqCdbj0udAM8-uOvh9y9Os_PIohalCiHnd1mBq8hPY0/edit?usp=sharing",
+      groupName: "Group B",
+      docIndex: 1,
+      url: "https://docs.google.com/document/d/1RqCdbj0udAM8-uOvh9y9Os_PIohalCiHnd1mBq8hPY0/edit?usp=sharing",
     },
     {
-        groupName: "Group C",
-        docIndex: 2,
-        url: "https://docs.google.com/document/d/1Lvi6oYtJeeFxNA-JaYRVp1O5rpnWbTMqHquvfGBV5Uc/edit?usp=sharing",
+      groupName: "Group C",
+      docIndex: 2,
+      url: "https://docs.google.com/document/d/1Lvi6oYtJeeFxNA-JaYRVp1O5rpnWbTMqHquvfGBV5Uc/edit?usp=sharing",
     },
-];
+  ];
   const [currDocIndex, setCurrDocIndex] = React.useState(0);
-  // create function to handle the next and previous buttons
+  const [view, setView] = React.useState("all"); // ["all", "single"
   const handleDocButton = (direction) => {
     if (direction === "right") {
       setCurrDocIndex((prevIndex) => (prevIndex + 1) % docUrls.length);
@@ -30,37 +30,57 @@ const docUrls = [
       );
     }
   };
-return (
+  return (
     <div>
+      {view === "all" && (
         <div>
+          <h2>All Docs View</h2>
+          {docUrls.map((doc, index) => (
+            <React.Fragment key={index}>
+              <iframe
+                className="teacher-iframe"
+                title={`group ${doc.docIndex}`}
+                src={doc.url}
+              ></iframe>
+            </React.Fragment>
+          ))}
+        </div>
+      )}
+      {view === "single" && (
+        <>
+          <div>
+            <h2>Single Doc View</h2>
             <button onClick={() => handleDocButton("left")}>Previous</button>
 
             {docUrls.map((doc, index) => (
-                <React.Fragment key={index}>
-                    {currDocIndex === index && (
-                        <iframe
-                            className="teacher-iframe"
-                            title={`group ${doc.docIndex}`}
-                            src={doc.url}
-                        ></iframe>
-                    )}
-                </React.Fragment>
+              <React.Fragment key={index}>
+                {currDocIndex === index && (
+                  <iframe
+                    className="teacher-iframe"
+                    title={`group ${doc.docIndex}`}
+                    src={doc.url}
+                  ></iframe>
+                )}
+              </React.Fragment>
             ))}
 
             <button onClick={() => handleDocButton("right")}>Next</button>
             {/* display currDocIndex */}
             <p>{currDocIndex}</p>
-        </div>
-        <div className="teacher-doc-buttons">
+          </div>
+          <div className="teacher-doc-buttons">
             {docUrls.map((doc, index) => (
-                <button key={index} onClick={() => setCurrDocIndex(index)}>
-                    {`${doc.groupName}`}
-                </button>
+              <button key={index} onClick={() => setCurrDocIndex(index)}>
+                {`${doc.groupName}`}
+              </button>
             ))}
-        </div>
+          </div>
+        </>
+      )}
+        <button onClick={() => setView("all")}>All Docs View</button>
+        <button onClick={() => setView("single")}>Single Doc View</button>
     </div>
-);
+  );
 };
 
 export default TeacherDocViewer;
-
--- socratic-circle-frontend/src/views/Teacher.js
+++ socratic-circle-frontend/src/views/Teacher.js
@@ -7,23 +7,6 @@ const Teacher = () => {
     <div>
       <h1>Teacher View</h1> {/* temp docs */}
       <div className="Teacher-Doc-Container">
-        <iframe
-          className="Teacher-Iframe"
-          title="group A"
-          src="https://docs.google.com/document/d/1OulQL0EtHAiTl7DEmLF77ApexEVsIQlwWdWr-4UQfhw/edit?usp=sharing"
-        ></iframe>
-        <iframe
-          className="Teacher-Iframe"
-          title="group B"
-          src="https://docs.google.com/document/d/1RqCdbj0udAM8-uOvh9y9Os_PIohalCiHnd1mBq8hPY0/edit?usp=sharing"
-        ></iframe>
-        <iframe
-          className="Teacher-Iframe"
-          title="group B"
-          src="https://docs.google.com/document/d/1Lvi6oYtJeeFxNA-JaYRVp1O5rpnWbTMqHquvfGBV5Uc/edit?usp=sharing"
-        ></iframe>
-      </div>
-      <div className="Teacher-Doc-Container">
         <TeacherDocViewer />
       </div>
     </div>
