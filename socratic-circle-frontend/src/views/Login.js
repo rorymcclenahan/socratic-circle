d1bb237cfd443a7cb329ad5f1d9b0692027a1020
rorymcc8898 <98715020+rorymcclenahan@users.noreply.github.com>
2024-03-30 23:07:11 -0400

    Create basic login/signup page

--- socratic-circle-frontend/src/views/Login.js
+++ socratic-circle-frontend/src/views/Login.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+const [isLogin, setIsLogin] = useState(true);
+
+const handleToggle = () => {
+    setIsLogin(!isLogin);
+};
+
+return (
+    <div>
+        {isLogin ? <h2>Login</h2> : <h2>Signup</h2>}
+        <form>
+            <label>
+                Email:
+                <input type="email" />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input type="password" />
+            </label>
+            <br />
+            {!isLogin && (
+                <label>
+                    Confirm Password:
+                    <input type="password" />
+                </label>
+            )}
+            <br />
+            <button type="submit">{isLogin ? 'Login' : 'Signup'}</button>
+        </form>
+        <button onClick={handleToggle}>
+            {isLogin ? 'Switch to Signup' : 'Switch to Login'}
+        </button>
+    </div>
+);
 }
 
 export default Login
