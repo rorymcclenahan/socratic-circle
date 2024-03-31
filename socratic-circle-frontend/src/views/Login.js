b2daf57fbba0bcc40bc9ce0f47cf550d3b653e78
rorymcc8898 <98715020+rorymcclenahan@users.noreply.github.com>
2024-03-31 02:38:27 -0400

    Add handle change to login

--- socratic-circle-frontend/src/views/Login.js
+++ socratic-circle-frontend/src/views/Login.js
@@ -1,44 +1,89 @@
-import React from 'react'
-import { useState } from 'react'
-import '../styles/Login.css'
+import React from "react";
+import { useState } from "react";
+import "../styles/Login.css";
 
 const Login = () => {
-const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(true);
 
-const handleToggle = () => {
-    setIsLogin(!isLogin);
-};
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-return (
-    <div className='login-menu'>
-        <div className='login-box'>
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    switch (name) {
+      case "username":
+        setUsername(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      case "confirmPassword":
+        setConfirmPassword(value);
+        break;
+      case "email":
+        setEmail(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className="login-menu">
+      <div className="login-box">
         {isLogin ? <h2>Login</h2> : <h2>Signup</h2>}
         <form>
-            <label className='entry-box'>
-                Email:
-                <input type="email" />
-            </label>
-            <br />
+          <label className="entry-box">
+            Username:
+            <input
+              type="email"
+              name="username"
+              value={email}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+          {!isLogin && (
             <label>
-                Password:
-                <input type="password" />
+              Confirm Password:
+              <input
+                type="password"
+                name="confirmPassword"
+                value={confirmPassword}
+                onChange={handleInputChange}
+              />
             </label>
-            <br />
-            {!isLogin && (
-                <label>
-                    Confirm Password:
-                    <input type="password" />
-                </label>
-            )}
-            <br />
-            <button type="submit">{isLogin ? 'Login' : 'Signup'}</button>
+          )}
+          {!isLogin && (
+            <label>
+              Email:
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={handleInputChange}
+              />
+            </label>
+          )}
+          <br />
+          <button type="submit">{isLogin ? "Login" : "Signup"}</button>
         </form>
-        <button onClick={handleToggle}>
-            {isLogin ? 'Switch to Signup' : 'Switch to Login'}
-        </button>
-        </div>
+      </div>
     </div>
-);
-}
+  );
+};
 
-export default Login
+export default Login;
