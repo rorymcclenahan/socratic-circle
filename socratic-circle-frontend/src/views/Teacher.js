edb770d0f0c8276dc5dc6d3fac00c126ec22dc77
rorymcc8898 <98715020+rorymcclenahan@users.noreply.github.com>
2024-03-30 22:43:05 -0400

    Add ability to toggle doc viewer in teacher

--- socratic-circle-frontend/src/views/Teacher.js
+++ socratic-circle-frontend/src/views/Teacher.js
@@ -3,12 +3,21 @@ import "../styles/iframe.css";
 import TeacherDocViewer from "../components/TeacherDocViewer";
 
 const Teacher = () => {
+  const [showTeacherDocViewer, setShowTeacherDocViewer] = React.useState(true);
+
+  const toggleTeacherDocViewer = () => {
+    setShowTeacherDocViewer(!showTeacherDocViewer);
+  };
   return (
     <div>
-      <h1>Teacher View</h1> {/* temp docs */}
-      <div className="Teacher-Doc-Container">
-        <TeacherDocViewer />
-      </div>
+      <button onClick={toggleTeacherDocViewer}>
+        Toggle Teacher Doc Viewer
+      </button>
+      {showTeacherDocViewer && (
+        <div className="Teacher-Doc-Container">
+          <TeacherDocViewer />
+        </div>
+      )}
     </div>
   );
 };
